53e9049d2000e81bf4d27b6a59901566e7366226
na2kera <keranatsuki@na2keraMacBook-Air.local>
2024-08-14 23:26:32 +0900

    画像アップロード

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,95 +1,107 @@
-import { Formik } from "formik";
+import Compressor from "compressorjs";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+  file: string;
+};
 
 const Signup = () => {
-  return (
-    <div>
-      <Formik
-        initialValues={{ name: "", email: "", password: "" }}
-        validate={(values) => {
-          const errors: {
-            name?: string;
-            email?: string;
-            password?: string;
-          } = {};
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = "Invalid email address";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data, event) => {
+    console.log(data);
+    const body = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    };
+    console.log(body);
+    // ユーザー登録処理
+    const res = await fetch("https://railway.bookreview.techtrain.dev/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const json = await res.json();
+    console.log(json);
+    const token = json.token;
+
+    // 画像アップロード処理
+    const fileInput = event?.target.file as HTMLInputElement;
+    const file = fileInput?.files?.[0];
+
+    if (file) {
+      new Compressor(file, {
+        quality: 0.6,
+        async success(result) {
+          const formData = new FormData();
+          console.log(result);
+          formData.append("icon", result as File, (result as File).name);
+          console.log(formData);
+          console.log(token);
+          try {
+            const response = await fetch(
+              "https://railway.bookreview.techtrain.dev/uploads",
+              {
+                method: "POST",
+                headers: {
+                  Authorization: `Bearer ${token}`, // トークンを使用して認証
+                },
+                body: formData,
+              }
+            );
+            const json2 = await response.json();
+            console.log(json2);
+          } catch (error) {
+            console.error(error);
           }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            fetch("https://railway.bookreview.techtrain.dev/users", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(values),
-            });
-            console.log(JSON.stringify(values));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="name">名前</label>
-            <br />
-            <input
-              type="text"
-              name="name"
-              id="name"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.name}
-            />
-            {errors.name && touched.name && errors.name}
-            <br />
-            <label htmlFor="email">メールアドレス</label>
-            <br />
-            <input
-              type="email"
-              name="email"
-              id="email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            {errors.email && touched.email && errors.email}
-            <br />
-            <label htmlFor="password">パスワード</label>
-            <br />
-            <input
-              type="password"
-              name="password"
-              id="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-            />
-            {errors.password && touched.password && errors.password}
-            <br />
-            <br />
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </form>
-        )}
-      </Formik>
-    </div>
+        },
+      });
+    }
+
+    console.log(file);
+  };
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <label htmlFor="name">Name</label>
+      <br />
+      <input {...register("name", { required: true })} />
+      <br />
+      {/* include validation with required or other standard HTML validation rules */}
+      <label htmlFor="email">Email</label>
+      <br />
+      <input {...register("email", { required: true })} />
+      <br />
+      <label htmlFor="password">Password</label>
+      <br />
+      <input {...register("password", { required: true })} />
+      <br />
+      <label htmlFor="file">File</label>
+      <br />
+      <input
+        {...register("file", { required: true })}
+        type="file"
+        id="file"
+        accept="image/jpg, image/png"
+      />
+
+      {/* errors will return when field validation fails  */}
+      {errors.email && <span>This field is required</span>}
+      <br />
+      <br />
+      <input type="submit" />
+    </form>
   );
 };
 
